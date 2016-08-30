b72d38a2dfd6a62618ad94201d65c69201de086b
Kyel John Davis <kyeljohnmdavid@gmail.com>
2016-08-30 21:19:16 +0800

    Removed thymeleaf dependencies

--- friflow-api/src/main/java/org/brightworks/friflow/Application.java
+++ friflow-api/src/main/java/org/brightworks/friflow/Application.java
@@ -1,14 +1,11 @@
 package org.brightworks.friflow;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 /**
  * @author kyel
@@ -25,16 +22,6 @@ public class Application {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).run();
-    }
-
-    /**
-     * This bean is automatically detected by our application
-     * It registers this bean as an additional dialect for Thymeleaf
-     * for enabling Spring securiy  tags on Thymeleaf templates
-     */
-    @Bean
-    public SpringSecurityDialect securityDialect() {
-        return new SpringSecurityDialect();
     }
 
 }
